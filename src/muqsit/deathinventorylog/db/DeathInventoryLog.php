e66edcb5e0b2afd6c134ad7ea381a5fa9a519a52
Jullius1392 <julllius1392@gmail.com>
2024-01-21 10:48:41 -0500

    Update DeathInventoryLog.php

--- src/muqsit/deathinventorylog/db/DeathInventoryLog.php
+++ src/muqsit/deathinventorylog/db/DeathInventoryLog.php
@@ -6,12 +6,18 @@ namespace muqsit\deathinventorylog\db;
 
 use Ramsey\Uuid\UuidInterface;
 
-final class DeathInventoryLog{
+final class DeathInventoryLog {
 
-	public function __construct(
-		readonly public int $id,
-		readonly public UuidInterface $uuid,
-		readonly public DeathInventory $inventory,
-		readonly public int $timestamp
-	){}
+    public int $id;
+    public UuidInterface $uuid;
+    public DeathInventory $inventory;
+    public int $timestamp;
+
+    public function __construct(int $id, UuidInterface $uuid, DeathInventory $inventory, int $timestamp) {
+        $this->id = $id;
+        $this->uuid = $uuid;
+        $this->inventory = $inventory;
+        $this->timestamp = $timestamp;
+    }
+
 }
