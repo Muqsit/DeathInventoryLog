703586639435ac4927fa184bb76e702abc98daa0
Muqsit <hackhack05@gmail.com>
2024-03-07 12:37:42 +0000

    Add Loader::onCommandAsync (async-equiv of onCommand)

--- src/muqsit/deathinventorylog/Loader.php
+++ src/muqsit/deathinventorylog/Loader.php
@@ -120,89 +120,96 @@ final class Loader extends PluginBase{
 	}
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+		Await::g2c($this->onCommandAsync($sender, $command, $label, $args));
+		return true;
+	}
+
+	/**
+	 * @param CommandSender $sender
+	 * @param Command $command
+	 * @param string $label
+	 * @param string[] $args
+	 * @return Generator<mixed, Await::RESOLVE, void, void>
+	 */
+	private function onCommandAsync(CommandSender $sender, Command $command, string $label, array $args) : Generator{
 		if(isset($args[0])){
 			switch($args[0]){
 				case "history":
 					if(!isset($args[1])){
 						$sender->sendMessage(TextFormat::RED . "/{$label} {$args[0]} <player> [page=1]");
-						return true;
+						return;
 					}
 
 					$page = isset($args[2]) ? max(1, (int) $args[2]) : 1;
 					$gamertags = [$args[1]];
-					Await::f2c(function() use($page, $sender, $gamertags) : Generator{
-						static $per_page = 10;
-						$offset = ($page - 1) * $per_page;
-						$translation = current(yield from Await::promise(function(Closure $resolve) use($gamertags) : void{
-							$this->getTranslator()->translateGamertags($gamertags, $resolve);
-						}));
-						$entries = $translation !== false ? yield from Await::promise(function(Closure $resolve) use($offset, $per_page, $translation) : void{
-							$this->database->retrievePlayer(Uuid::fromBytes($translation), $offset, $per_page, $resolve);
-						}) : [];
+					static $per_page = 10;
+					$offset = ($page - 1) * $per_page;
+					$translation = current(yield from Await::promise(function(Closure $resolve) use($gamertags) : void{
+						$this->getTranslator()->translateGamertags($gamertags, $resolve);
+					}));
+					$entries = yield from $this->database->retrievePlayerAsync(Uuid::fromBytes($translation), $offset, $per_page);
 
-						/** @var DeathInventoryLog[] $entries */
-						if(count($entries) === 0){
-							$sender->sendMessage($page === 1 ? TextFormat::RED . "No logs found for that player." : TextFormat::RED . "No logs found on page {$page} for that player.");
-							return;
-						}
+					/** @var DeathInventoryLog[] $entries */
+					if(count($entries) === 0){
+						$sender->sendMessage($page === 1 ? TextFormat::RED . "No logs found for that player." : TextFormat::RED . "No logs found on page {$page} for that player.");
+						return;
+					}
 
-						if($sender instanceof Player && !$sender->isConnected()){
-							return;
-						}
+					if($sender instanceof Player && !$sender->isConnected()){
+						return;
+					}
 
-						$message = TextFormat::BOLD . TextFormat::RED . "Death Entries Page {$page}" . TextFormat::RESET . TextFormat::EOL;
-						foreach($entries as $entry){
-							$message .= TextFormat::BOLD . TextFormat::RED . ++$offset . ". " . TextFormat::RESET . TextFormat::WHITE . "#{$entry->id} " . TextFormat::GRAY . "logged on " . gmdate("Y-m-d H:i:s", $entry->timestamp) . TextFormat::EOL;
-						}
-						$sender->sendMessage($message);
-					});
-					return true;
+					$message = TextFormat::BOLD . TextFormat::RED . "Death Entries Page {$page}" . TextFormat::RESET . TextFormat::EOL;
+					foreach($entries as $entry){
+						$message .= TextFormat::BOLD . TextFormat::RED . ++$offset . ". " . TextFormat::RESET . TextFormat::WHITE . "#{$entry->id} " . TextFormat::GRAY . "logged on " . gmdate("Y-m-d H:i:s", $entry->timestamp) . TextFormat::EOL;
+					}
+					$sender->sendMessage($message);
+					return;
 				case "retrieve":
 					if(!($sender instanceof Player)){
 						$sender->sendMessage(TextFormat::RED . "This command can only be executed in-game.");
-						return true;
+						return;
 					}
 
 					if(!isset($args[1])){
 						$sender->sendMessage(TextFormat::RED . "/{$label} {$args[0]} <id>");
-						return true;
+						return;
 					}
 
 					$id = (int) $args[1];
-					$this->database->retrieve($id, static function(?DeathInventoryLog $log) use ($sender, $id) : void{
-						if(!$sender->isConnected()){
-							return;
+					$log = yield from $this->database->retrieveAsync($id);
+					if(!$sender->isConnected()){
+						return;
+					}
+
+					if($log === null){
+						$sender->sendMessage(TextFormat::RED . "No death log with the id #{$id} could be found.");
+						return;
+					}
+
+					$sender->sendMessage(TextFormat::GRAY . "Retrieved death inventory log #{$log->id}");
+
+					$items = $log->inventory->inventory_contents;
+
+					$armor_inventory = $log->inventory->armor_contents;
+					foreach([
+						ArmorInventory::SLOT_HEAD => 47,
+						ArmorInventory::SLOT_CHEST => 48,
+						ArmorInventory::SLOT_LEGS => 50,
+						ArmorInventory::SLOT_FEET => 51
+					] as $armor_inventory_slot => $menu_slot){
+						if(isset($armor_inventory[$armor_inventory_slot])){
+							$items[$menu_slot] = $armor_inventory[$armor_inventory_slot];
 						}
+					}
 
-						if($log === null){
-							$sender->sendMessage(TextFormat::RED . "No death log with the id #{$id} could be found.");
-							return;
-						}
+					$items[53] = $log->inventory->offhand_contents[0] ?? VanillaItems::AIR();
 
-						$sender->sendMessage(TextFormat::GRAY . "Retrieved death inventory log #{$log->id}");
-
-						$items = $log->inventory->inventory_contents;
-
-						$armor_inventory = $log->inventory->armor_contents;
-						foreach([
-							ArmorInventory::SLOT_HEAD => 47,
-							ArmorInventory::SLOT_CHEST => 48,
-							ArmorInventory::SLOT_LEGS => 50,
-							ArmorInventory::SLOT_FEET => 51
-						] as $armor_inventory_slot => $menu_slot){
-							if(isset($armor_inventory[$armor_inventory_slot])){
-								$items[$menu_slot] = $armor_inventory[$armor_inventory_slot];
-							}
-						}
-
-						$items[53] = $log->inventory->offhand_contents[0] ?? VanillaItems::AIR();
-
-						$menu = InvMenu::create(InvMenu::TYPE_DOUBLE_CHEST);
-						$menu->setName("Death Inventory Log #{$log->id}");
-						$menu->getInventory()->setContents($items);
-						$menu->send($sender);
-					});
-					return true;
+					$menu = InvMenu::create(InvMenu::TYPE_DOUBLE_CHEST);
+					$menu->setName("Death Inventory Log #{$log->id}");
+					$menu->getInventory()->setContents($items);
+					$menu->send($sender);
+					return;
 			}
 		}
 
@@ -211,6 +218,5 @@ final class Loader extends PluginBase{
 			TextFormat::WHITE . "/{$label} history <player> [page=1] " . TextFormat::GRAY . "Retrieve a player's death log history" . TextFormat::EOL .
 			TextFormat::WHITE . "/{$label} retrieve <id> " . TextFormat::GRAY . "Retrieve inventory contents of a specific death log"
 		);
-		return true;
 	}
 }
